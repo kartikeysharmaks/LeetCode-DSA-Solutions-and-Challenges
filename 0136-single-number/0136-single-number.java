6c5e34b1f8696822c78dfaa59743bae39736b0e8
Kartikey Sharma <kartikeysharma1616@gmail.com>
2022-11-08 22:59:41 +0530

    Time: 19 ms (30.93%), Space: 54 MB (17.62%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashSet<Integer> set = new HashSet<Integer>();
+        for(int i : nums){
+            if(set.contains(i)){
+                set.remove(i);
+            }else{
+                set.add(i);
+            }
+        }
+        
+        for(int i : set){
+            return i;
+        }
+        
+        return -1;
+    }
+}
